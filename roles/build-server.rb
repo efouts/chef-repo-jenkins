3b5c41a206d19757f7c0e1b6e1285fe30172a934
Elliott Fouts <efouts@ccs-inc.com>
2012-02-10 22:55:43 -0500

    Changed from json to rb format

--- roles/build-server.rb
+++ roles/build-server.rb
@@ -1,19 +1,3 @@
-{
-  "name": "build-server",
-  "default_attributes": {
-  },
-  "json_class": "Chef::Role",
-  "env_run_lists": {
-  },
-  "run_list": [
-    "recipe[apt]",
-    "recipe[git]",
-    "recipe[runit]",
-    "recipe[java]",
-    "recipe[jenkins]"
-  ],
-  "description": "",
-  "chef_type": "role",
-  "override_attributes": {
-  }
-}
+name 'build-server'
+description 'Jenkins build server'
+run_list 'recipe[apt]', 'recipe[git]', 'recipe[runit]', 'recipe[java]', 'recipe[jenkins]'
