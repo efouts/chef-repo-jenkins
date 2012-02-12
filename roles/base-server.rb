9b991c02860e3f1f09be543d78070bcdbdcc3ac7
Elliott Fouts <efouts@ccs-inc.com>
2012-02-11 21:26:41 -0500

    Restored apt recipe to base-server role

--- roles/base-server.rb
+++ roles/base-server.rb
@@ -1,4 +1,3 @@
 name 'base-server'
 description 'Base role for all servers'
-run_list 'recipe[sudo]', 'recipe[users::sysadmins]', 'recipe[openssh]'
-#run_list 'recipe[apt]', 'recipe[openssh]', 'recipe[users::sysadmins]'
+run_list 'recipe[apt]', 'recipe[sudo]', 'recipe[users::sysadmins]', 'recipe[openssh]'
