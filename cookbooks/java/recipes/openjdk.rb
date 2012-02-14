5a1e51f01914e24faffd35da7bd94b4b3c84750d
Elliott Fouts <efouts@ccs-inc.com>
2012-02-13 22:36:12 -0500

    Import java version 1.1.0

--- cookbooks/java/recipes/openjdk.rb
+++ cookbooks/java/recipes/openjdk.rb
@@ -21,24 +21,11 @@ pkgs = value_for_platform(
   ["centos","redhat","fedora"] => {
     "default" => ["java-1.6.0-openjdk","java-1.6.0-openjdk-devel"]
   },
-  ["arch","freebsd"] => {
-    "default" => ["openjdk6"]
-  },
   "default" => ["openjdk-6-jdk","default-jdk"]
 )
 
-java_alternative_name = value_for_platform(
-  ["ubuntu"] => {
-    "11.10"   => "java-1.6.0-openjdk",
-    "default" => "java-6-openjdk"
-  },
-  ["debian"] => {
-    "default" => "java-6-openjdk"
-  }
-)
-
 execute "update-java-alternatives" do
-  command "update-java-alternatives -s #{java_alternative_name}"
+  command "update-java-alternatives -s java-6-openjdk"
   returns [0,2]
   action :nothing
   only_if { platform?("ubuntu", "debian") }
